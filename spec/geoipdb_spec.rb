f1bf498b7a60a3025aa333783952e3abe802bd53
Eugen Martin <eugen.martin@madvertise.de>
2011-01-13 17:17:51 +0100

    some specs

--- spec/geoipdb_spec.rb
+++ spec/geoipdb_spec.rb
@@ -1,7 +1,37 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__)+'/spec_helper')
+
 
 describe "Geoipdb" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  def init_db
+    @db = GeoIpDb.init './sample_data/cities.csv', './sample_data/ip_ranges.csv', @cache_file
+  end
+  
+  before :each do
+    @cache_file = 'sample_data/ipdb.cache'
+  end
+  
+  it "should init correctly with sample data and create the cache-file" do
+    init_db
+    @db.should_not be_nil
+    File.exist?(@cache_file).should be_true
+  end
+  
+  it "sould find the sample cities correcty" do 
+    init_db
+    #afg,no region,kabul,-1,3,34.5167,69.1833
+    @db.city_by_ip("1.1.0.254").should == {'city_code'=>3, 'name'=>'kabul', 'country'=>'af', 'lat'=>34.5167, 'lng'=>69.1833}
+  end
+  
+  it "should not throw an exception fault if data is corrupt" do
+    @db = GeoIpDb.init './sample_data/cities_corrupt.csv', './sample_data/ip_ranges_corrupt.csv', @cache_file    
+  end
+
+  it "should not init a db object if data files are missing" do
+    GeoIpDb.init( './sample_data/bla.csv', './sample_data/blubb.csv', @cache_file    ).should be_nil
+  end
+
+  
+  after :each do
+    File.unlink @cache_file if File.exist? @cache_file
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'ext'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'geoipdb'
