0b44e3e067d9be5c089622bb5e8c783a29c50947
Martin Karlsch <martin@karlsch.com>
2011-03-11 15:45:08 +0100

    - fixed specs to work with new structure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'ext'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
-require 'lib/ipdb'
+require 'lib/geoipdb'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
