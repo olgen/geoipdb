57c3866eb89148924b00d9dad4a9599548bf7fa0
Benedikt Böhm <benedikt.boehm@madvertise.com>
2013-01-10 14:18:46 +0100

    add to_h for IpInformation

--- lib/ip_information.rb
+++ lib/ip_information.rb
@@ -1,15 +1,27 @@
 class IpInformation
 
-  attr_accessor :country_iso_code
-  attr_accessor :city_code
-  attr_accessor :city_name
-  attr_accessor :lat
-  attr_accessor :lng
-  attr_accessor :is_mobile
-  attr_accessor :isp_name
+  ATTRIBS = [
+    :country_iso_code,
+    :city_code,
+    :city_name,
+    :lat,
+    :lng,
+    :is_mobile,
+    :isp_name,
+  ]
+
+  ATTRIBS.each do |attrib|
+    attr_accessor attrib
+  end
 
   def mobile?
     @is_mobile
+  end
+
+  def to_h
+    Hash[ATTRIBS.map do |attrib|
+      [attrib, send(attrib)]
+    end]
   end
 
 end
